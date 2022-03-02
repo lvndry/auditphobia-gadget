c82864727c5949e47bfdebea4cb1c4cba13f942b
lvndry <lvndry@protonmail.com>
2022-03-02 22:04:17 +0100

    add eslint and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: ["./tsconfig.json"] },
+  tsconfigDir: ".",
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+};

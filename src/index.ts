82ed905058110d60ae7bfb6f3be0ca33ba6a56b4
lvndry <lvndry@protonmail.com>
2022-03-21 22:57:55 +0100

    remove unused main function

--- src/index.ts
+++ src/index.ts
@@ -140,11 +140,3 @@ export const generatePackageAudit = async (
     return formatOutput(stdout);
   }
 };
-
-(async () => {
-  const audit = await generatePackageAudit({
-    name: "create-react-app",
-    version: "5.0.0",
-  });
-  console.log(JSON.stringify(audit, null, 2));
-})();
